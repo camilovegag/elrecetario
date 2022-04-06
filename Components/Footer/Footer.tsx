ce4a4a47b9f6f88cc885b89da62ff20ba4ea6264
Camilo Vega <camilovegag@unisabana.edu.co>
2022-04-06 16:24:53 -0500

    Modify Footer files

---
+++ Components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <h4>El Recetario - All rights reserved © 2022</h4>
+  </footer>
+);
+
+export default Footer;
