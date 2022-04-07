19f31e48f6aa9a73c937703e8d9edc9c6223d584
Rodrigo <rodrigohp4@hotmail.com>
2022-04-06 19:56:10 -0500

    fix folders,change styles ,fix card and replace Image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Card from "../Components/Card";
+import Card from "../Components/card";
 
 const Home: NextPage = () => {
   return (
